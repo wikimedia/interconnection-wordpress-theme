d6162690960d61c5459884201ae16ee4e3318ed4
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2022-01-17 06:38:29 -0800

    Add comment to explain use of wp object

--- js/editor.js
+++ js/editor.js
@@ -2,6 +2,11 @@
  * File editor.js.
  *
  * Core block modifications and styles.
+ *
+ * NOTE: We are temporarily using the wp object in the browser to avoid having to setup
+ * Webpack and npm to bundle block files since we are only adding two block styles
+ * to the Group block. However, if we continue to develop more complex block solutions,
+ * we would need to setup the theme to handle modern block development.
  */
 wp.domReady( function() {
 	const __ = wp.i18n.__;
