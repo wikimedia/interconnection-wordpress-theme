18b6f757ff25a3cf7197a264f99c315d7cadfe96
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2022-06-13 18:42:31 -0700

    Add aria-label with language slug

--- template-parts/content-grid.php
+++ template-parts/content-grid.php
@@ -7,11 +7,22 @@
  * @package Interconnection
  */
 
+$lang_attribute = '';
+$lang_title     = '';
+$lang_slug      = pll_get_post_language( get_the_ID(), 'slug' );
+$lang_name      = pll_get_post_language( get_the_ID(), 'name' );
+
+if ( pll_current_language() !== $lang_slug ) {
+	$lang_attribute = 'lang="' . $lang_slug . '"';
+	$lang_title     = '<span aria-label="(' . $lang_name . ')">[' . strtoupper( $lang_slug ) . ']</span> ';
+}
+
 ?>
 
-<article class="grid-post" id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+<article class="grid-post" id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php echo esc_attr( $lang_attribute ); ?>>
 	<header class="entry-header">
 		<?php interconnection_post_thumbnail(); ?>
+		<?php echo esc_html( $lang_title ); ?>
 		<?php the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' ); ?>
 	</header><!-- .entry-header -->
 
