5f9032b720986bbf400c7b8f8364a61c52722d71
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2022-06-15 22:24:11 -0700

    Fix permalink button URL

--- template-parts/modules/featured-post.php
+++ template-parts/modules/featured-post.php
@@ -7,16 +7,17 @@
  * @package Interconnection
  */
 
-?>
+$permalink = get_permalink();
 
+?>
 <article class="featured-post grid-post" id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
 	<div class="featured-post-image">
 		<?php interconnection_featured_post_thumbnail(); ?>
 	</div>
 
-	<div class="featured-post-text">	
+	<div class="featured-post-text">
 		<header class="entry-header">
-			<?php the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' ); ?>
+			<?php the_title( '<h3 class="entry-title"><a href="' . esc_url( $permalink ) . '" rel="bookmark">', '</a></h3>' ); ?>
 		</header><!-- .entry-header -->
 
 		<?php if ( 'post' === get_post_type() ) : ?>
@@ -28,21 +29,22 @@
 			</div><!-- .entry-meta -->
 			<div class="post-excerpt">
 				<p>
-					<?php 
-					$limit = 20;
-					$excerpt = explode(' ', get_the_excerpt(), $limit);
-						if (count($excerpt)>=$limit) {
-							array_pop($excerpt);
-							$excerpt = implode(" ", $excerpt) . '...';
-						} else {
-							$excerpt = implode(" ", $excerpt);
-						}	
-					$excerpt = preg_replace('`[[^]]*]`', '', $excerpt);
-					echo wp_strip_all_tags( $excerpt );
-					?>	
+					<?php
+					$limit   = 20;
+					$excerpt = explode( ' ', get_the_excerpt(), $limit );
+
+					if ( count( $excerpt ) >= $limit ) {
+						array_pop( $excerpt );
+						$excerpt = implode( ' ', $excerpt ) . '...';
+					} else {
+						$excerpt = implode( ' ', $excerpt );
+					}
+
+					$excerpt = preg_replace( '`[[^]]*]`', '', $excerpt );
+					echo esc_html( wp_strip_all_tags( $excerpt ) );
+					?>
 				</p>
-				<!-- ATTENTION: Needs translation -->
-				<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark" class="btn btn-accent">Read more</a>
+				<a href="<?php echo esc_url( $permalink ); ?>" rel="bookmark" class="btn btn-accent">Read more</a>
 			</div>
 		<?php endif; ?>
 	</div>
