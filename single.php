6cebd737b50285b58f66241d27a0dc9d5b5adce7
Alexis J. Villegas <14046668+ajvillegas@users.noreply.github.com>
2022-06-21 19:40:14 -0700

    Update syntax and formatting

--- single.php
+++ single.php
@@ -11,32 +11,34 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-			
-			<?php
-			while ( have_posts() ) :
-				the_post();
-				get_template_part( 'template-parts/content', get_post_type() );
-			?>
+		<?php
+		while ( have_posts() ) :
+			the_post();
+			get_template_part( 'template-parts/content', get_post_type() );
 
+			?>
 			<div class="wrapper">
 
-				<?php if ( class_exists( 'Jetpack_RelatedPosts' ) ) { ?>
+				<?php
+				if ( class_exists( 'Jetpack_RelatedPosts' ) ) :
+					?>
 					<div class="jetpack-related-posts">
 						<?php echo do_shortcode( '[jetpack-related-posts]' ); ?>
 					</div>
-				<?php } else {
+					<?php
+				else :
 					the_post_navigation(
 						array(
 							'prev_text' => '<span class="nav-subtitle">' . esc_html__( '', 'interconnection' ) . '</span> <span class="nav-title">← %title</span>',
 							'next_text' => '<span class="nav-subtitle">' . esc_html__( '', 'interconnection' ) . '</span> <span class="nav-title">%title →</span>',
 						)
 					);
-				}; ?>
+				endif;
+				?>
 			</div>
-
-			<?php endwhile; // End of the loop. 
-			?>
-
+			<?php
+		endwhile; // End of the loop.
+		?>
 	</main><!-- #main -->
 
 <?php
