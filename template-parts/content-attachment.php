babb8fef7e180112d2d870ec4624f29de6e4389f
K. Adam White <kadamwhite@users.noreply.github.com>
2023-08-29 17:38:11 -0700

    Output image credit attribution on attachment page

--- template-parts/content-attachment.php
+++ template-parts/content-attachment.php
@@ -7,6 +7,12 @@
  * @package Interconnection
  */
 
+$credit_info  = Interconnection\Credits::get_image_credits( get_the_ID() );
+$author       = $credit_info['author'] ?? '';
+$license      = $credit_info['license'] ?? '';
+$license_url  = $credit_info['license_url'] ?? '';
+$url          = $credit_info['url'];
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -17,6 +23,22 @@
 			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		</header>
 
+		<div class="entry-meta">
+			<?php if ( ! empty( $author ) ) : ?>
+				<p class="credit"><?php echo esc_html( $author ); ?></p>
+			<?php endif; ?>
+
+			<?php if ( ! empty( $license ) ) : ?>
+				<p class="credit-desc" >
+					<?php if ( ! empty( $license_url ) ) : ?>
+						<a href="<?php echo esc_url( $license_url ); ?>" target="_blank">
+					<?php endif; ?>
+							<?php echo esc_html( $license ); ?>
+					<?php if ( ! empty( $license_url ) ) : ?>
+						</a>
+					<?php endif; ?></p>
+			<?php endif; ?>
+		</div>
 		<div class="entry-content">
 			<div class="main-entry-content">
 				<?php
